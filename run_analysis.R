a2ae042443fc20bf37581def4261f8434bb1cb3f
takashisan1 <52939888+takashisan1@users.noreply.github.com>
2019-09-08 19:29:39 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,21 +15,21 @@ variable_names <- read.table("./UCI HAR Dataset/features.txt")
 # read activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# 1. Merges the training and the test sets to create one data set.
+# 1.Merges the training and the test sets to create one data set
 X_total <- rbind(X_train, X_test)
 Y_total <- rbind(Y_train, Y_test)
 Sub_total <- rbind(Sub_train, Sub_test)
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 2.Extracts only the measurements on the mean and standard deviation for each measurement
 selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
 X_total <- X_total[,selected_var[,1]]
-# 3. Uses descriptive activity names to name the activities in the data set
+# 3.Uses descriptive activity names to name the activities in the data set
 colnames(Y_total) <- "activity"
 Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
 activitylabel <- Y_total[,-1]
-# 4. Appropriately labels the data set with descriptive variable names.
+# 4.Appropriately labels the data set with descriptive variable names
 colnames(X_total) <- variable_names[selected_var[,1],2]
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average
-# of each variable for each activity and each subject.
+
+# 5.From the data set in step 4, creates a second, independent tidy data set with the average
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
